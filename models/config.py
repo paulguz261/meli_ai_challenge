412d743046962acd6a84d6abca0cadab6b5d4e2d
paulguz261 <paulguz261@gmail.com>
2025-06-15 23:04:44 -0500

    :wrench: change config ML model exports name

--- models/config.py
+++ models/config.py
@@ -10,6 +10,6 @@ TRAIN_DIR = os.path.join(DATA_DIR, TRAIN_DATA_NAME)
 TEST_DIR = os.path.join(DATA_DIR, TEST_DATA_NAME)
 
 # Model location
-MODEL_BASE_NAME = "cybersecurity_model_v{version}"
+MODEL_BASE_NAME = "cybersecurity_model_v{version}.joblib"
 MODEL_DIR = os.path.join(PROJECT_DIR, 'models')
 MODEL_EXPORT_DIR = os.path.join(MODEL_DIR, 'exports')
